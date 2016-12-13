2ca784b9eda12bdf0b42d4055f7968b5f3e189e0
Erik Gúth <guthember@gmail.com>
2016-12-13 14:56:32 +0100

    Update program.cs

--- program.cs
+++ program.cs
@@ -44,6 +44,32 @@ namespace caNevek
                 Console.WriteLine("Sikertelen");
             }
 
+            string query = "SELECT * FROM nevek";
+
+            List<string>[] list = new List<string>[3];
+            list[0] = new List<string>(); // id
+            list[1] = new List<string>(); // nev 
+            list[2] = new List<string>(); // cim
+
+            MySqlCommand cmd = new MySqlCommand(query, connection);
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                list[0].Add(dataReader["nevid"] + "");
+                list[1].Add(dataReader["nev"] + "");
+                list[2].Add(dataReader["cim"] + "");
+            }
+
+            dataReader.Close();
+
+            for (int i = 0; i <= list.Length; i++)
+            {
+                Console.WriteLine(list[0][i]+" "+list[1][i].ToString().PadRight(20)+
+                    " "+list[2][i].ToString().PadRight(40));
+            }
+
+            
             connection.Close();
 
             Console.ReadKey();
